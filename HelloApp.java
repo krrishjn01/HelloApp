4a52581642d18b83277b45b20de887b846ec9bf6
krrishjn01 <kj8208@srmist.edu.in>
2026-03-27 16:24:50 +0530

    Coded UC5: Implement enhanced for-loop for greeting multiple users

--- HelloApp.java
+++ HelloApp.java
@@ -1,7 +1,14 @@
 public class HelloApp {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
-        String name = (args.length == 0) ? "World" : String.join(", ", args);
-        System.out.println("Hello, " + name + "!");
+        // Check if no arguments are passed
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            // Enhanced for loop (for-each)
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
+        }
     }
 }
